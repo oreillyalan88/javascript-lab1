0e1aa55b382d36030064e240a20dddb1578b2137
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-20 19:48:52 +0000

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,7 @@
     var aCar = {
         owner : "Joe Bloggs",
+        previous_owners : [ { name : 'Pat Smith', address : '1 Main Street'},
+                            { name : 'Sheila Dwyer', address : '2 High Street'}],
         type : {make :'Toyota',
                 model : 'Corolla',
                 cc : '1.8'},
@@ -20,9 +22,10 @@
     
         
     aCar.mileage = 80000; 
+    aCar.address ="123 Fake Street"
     aCar.color = layout;
-// aCar.color.interior.textrue = 'leather';
-// aCar.color.interior.shade = 'crean';
-
-
-console.log('Its a '+aCar.color.exterior+" car, "+aCar.mileage+", and "+aCar.color.interior.texture+' interior.' );
+    
+    
+console.log('First owner : ' + aCar.previous_owners[0].name+" and "+aCar.previous_owners[0].address);
+console.log('Second owner : ' + aCar.previous_owners[1].name+" and "+aCar.previous_owners[1].address);
+console.log(aCar);
