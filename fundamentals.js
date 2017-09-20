e6de9350a50d3f339ec7db7bed85fef5e7e492c3
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-20 19:28:07 +0000

    Dynamic Properties of js

--- fundamentals.js
+++ fundamentals.js
@@ -7,7 +7,22 @@
                         county : 'WD',
                         number : '1058'}
     };
-    
 
-console.log(aCar.owner + ' drives a ' + aCar.type.make);
-console.log('Reg. = ' + aCar.registration.year + '-'+ aCar.registration.county + '-'+ aCar.registration.number+'.');
+    
+    var layout = {
+        exterior : 'red',
+        interior: {
+            texture : 'leather',
+            shade : 'cream'
+        }
+        
+    };
+    
+        
+    aCar.mileage = 80000; 
+    aCar.color = layout;
+// aCar.color.interior.textrue = 'leather';
+// aCar.color.interior.shade = 'crean';
+
+
+console.log('Its a '+aCar.color.exterior+" car, "+aCar.mileage+", and "+aCar.color.interior.texture+' interior.' );
