a004d66ee2c5db123f885150e59c8b28ac7d8356
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-20 19:57:28 +0000

    Loop Constructs

--- fundamentals.js
+++ fundamentals.js
@@ -5,6 +5,11 @@
         type : {make :'Toyota',
                 model : 'Corolla',
                 cc : '1.8'},
+                
+        features : ['Parking assist', 
+                    'Alarm', 
+                    'Tow-bar'],
+                    
         registration : {year :'10',
                         county : 'WD',
                         number : '1058'}
@@ -25,7 +30,19 @@
     aCar.address ="123 Fake Street"
     aCar.color = layout;
     
+    for (var i = 0 ; i < aCar.features.length ; i += 1) {
+        console.log(aCar.features[i]) ;
+    } 
     
-console.log('First owner : ' + aCar.previous_owners[0].name+" and "+aCar.previous_owners[0].address);
-console.log('Second owner : ' + aCar.previous_owners[1].name+" and "+aCar.previous_owners[1].address);
-console.log(aCar);
+    for (var i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+        console.log(aCar.previous_owners[i].name) ;
+    } 
+    
+    
+    for (var p in aCar.type)  {
+        console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+    }
+    
+    console.log('First owner : ' + aCar.previous_owners[0].name+" and "+aCar.previous_owners[0].address);
+    console.log('Second owner : ' + aCar.previous_owners[1].name+" and "+aCar.previous_owners[1].address);
+    console.log(aCar);
